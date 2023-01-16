4658f7e54080af5e1e66f0a5e0e68028cfa070cf
Wendy Miller <weendymilleer@gmail.com>
2023-01-16 09:52:54 -0300

    dependencias

--- desafio/export.js
+++ desafio/export.js
@@ -1,6 +1,9 @@
 module.exports = function getFlag(string){
+    
     let index = process.argv.indexOf(string) + 1
+    console.log(process.argv[index-1])
     if(index != -1){
+        console.log(process.argv[index])
         return process.argv[index]
     }
     return 'não há essa flag aqui'
