412b20e992b5f5e1c1e6d8ed9e5184a3178a3825
Wendy Miller <weendymilleer@gmail.com>
2023-01-17 12:10:39 -0300

    app

--- BestMe/index.js
+++ BestMe/index.js
@@ -8,15 +8,24 @@ const questions = [
 ]
 
 const ask = (index = 0) => {
-    return process.stdout.write(questions[index] + "\n\n")
+    return process.stdout.write("\n\n" + questions[index] + " > ")
 }
 
 ask()
 
+
+const answers = []
 //on é alguma coisa que fica ouvindo eventos
 
 process.stdin.on("data", data => {
-    process.stdout.write(data.toString().trim() + '\n')//transforma em string e tira espaços vazios
-    process.exit()//senao processo fica sempre esperando entradas de dados
+    answers.push(data.toString().trim())
+
+    if(answers.length <= questions.length){
+        ask(answers.length)
+    }else{
+        console.log(answers)
+        process.exit()//senao processo fica sempre esperando entradas de dados
+    }
+    
 })//"data" ele esta ouvindo quando tiver dados, e toda vez que for inserido dados ele vai rodar uma função
 
