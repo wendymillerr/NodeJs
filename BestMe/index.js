8befb822db4c028d3ff8ab4772844b0af3b26274
Wendy Miller <weendymilleer@gmail.com>
2023-01-17 11:47:21 -0300

    stdin

--- BestMe/index.js
+++ BestMe/index.js
@@ -12,3 +12,11 @@ const ask = (index = 0) => {
 }
 
 ask()
+
+//on é alguma coisa que fica ouvindo eventos
+
+process.stdin.on("data", data => {
+    process.stdout.write(data.toString().trim() + '\n')//transforma em string e tira espaços vazios
+    process.exit()//senao processo fica sempre esperando entradas de dados
+})//"data" ele esta ouvindo quando tiver dados, e toda vez que for inserido dados ele vai rodar uma função
+
