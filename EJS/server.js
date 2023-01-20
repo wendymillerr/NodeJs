b0edcf5a92f7e8e93b3730e92b14a2bc55e3eccd
Wendy Miller <weendymilleer@gmail.com>
2023-01-20 08:43:45 -0300

    passando objetos para o ejs

--- EJS/server.js
+++ EJS/server.js
@@ -4,11 +4,11 @@ const app = express(); //instanciando express
 //a ferramenta que a gnt vai usar para renderizar html(view enfine) será o ejs
 app.set("view engine", "ejs");//metodo do express
 app.get("/", function(reg, res){//colocando rota. ex: jaqueline.com.br
-    res.render("index");//resposta é renderizar o index. n precisa de "ejs" pq falou ali em cima que 
+    res.render("pages/index");//resposta é renderizar o index. n precisa de "ejs" pq falou ali em cima que 
 })
 
 app.get("/sobre", function(reg, res){//colocando rota. ex: jaqueline.com.br
-    res.render("about");//resposta é renderizar o index. n precisa de "ejs" pq falou ali em cima que 
+    res.render("pages/about");//resposta é renderizar o index. n precisa de "ejs" pq falou ali em cima que 
 })
 
 app.listen(8080);//função do express que fica ouvind uma porta(8080). quando chamar essa porta vai executar todo este arquivo
